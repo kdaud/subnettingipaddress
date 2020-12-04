8f5c793eb3d74cee474edd8dfb9ac10b4fbbe9ce
kdaud <dkakumirizii@gmail.com>
2020-12-04 14:56:33 +0300

    javadocs api

---
+++ src/com/network/mask/Inet.java
@@ -0,0 +1,29 @@
+package com.network.mask;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+public class Inet {
+	
+	public static void mySubnet() throws UnknownHostException {
+		Inet4Address obj = (Inet4Address) Inet4Address.getLocalHost();
+		String myipaddres = obj.getCanonicalHostName();
+		String myHostAddress = obj.getHostAddress();
+		String myHostName = obj.getHostName();
+		
+		System.out.println("Ip Address: " + obj.getHostAddress().trim());
+		System.out.println(myipaddres.toString() + " " + myHostAddress + " " + myHostName);
+		
+	}
+	
+	public static void main(String[] args) {
+		try {
+			mySubnet();
+		}
+		catch (UnknownHostException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
+	}
+	
+}
